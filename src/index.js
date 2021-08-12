c30533a06f6a4db14160f5f532102fb17dd0cbd0
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-08-12 16:32:59 +0530

    added serviceWorker file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <HashRouter>
@@ -13,4 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
